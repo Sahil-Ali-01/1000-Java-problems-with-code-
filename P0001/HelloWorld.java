e101f3894f73638894675dcd095efe725c797b95
Sahil Ali <sahilcoder189@gmail.com>
2024-09-24 05:40:44 +0530

    Add files via upload

---
+++ P0001/HelloWorld.java
@@ -0,0 +1,8 @@
+ /**
+ * Java Program to Print Hello World on Screen
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
